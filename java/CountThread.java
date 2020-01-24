d03d8a5b29cb62c7e41aae8c2805126942d96d2d
George Porter <gmporter@cs.ucsd.edu>
2020-01-23 16:39:12 -0800

    A java thread example

---
+++ java/CountThread.java
@@ -0,0 +1,29 @@
+
+public class CountThread extends Thread {
+	private String preamble = "";
+
+	public CountThread(int tnum) {
+		super("t"+tnum);
+		System.out.println("Starting thread " + this.getName());
+		preamble = new String(new char[tnum]).replace("\0", "\t");
+	}
+
+	private void mySleep(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) { }
+	}
+
+	public void run() {
+		for( int i = 0; i < 10; i++ ) {
+			mySleep(500);  // milliseconds
+			System.out.println(preamble + this.getName() + ":" + i );
+		}
+	}
+
+	public static void main(String[] args) {
+		Thread t1 = new CountThread(1);
+		Thread t2 = new CountThread(2);
+		t1.start(); t2.start();
+	}
+}
